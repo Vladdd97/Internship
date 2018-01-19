bea0704a69ee45a43316cd3cab10afc8036be336
Naxgo <bezualex@gmail.com>
2018-01-19 12:44:08 +0200

    Began extending functionality for adding start and end points for the route

--- www/script/main.js
+++ www/script/main.js
@@ -1,14 +1,13 @@
 $(document).ready(main);
+var geocoder;
+var markers = [] ;
+var map;
 
 function main() {
-    $("#addMarker").click(addMarker);
     myMap(parseFloat(47.010921), parseFloat(28.840330));
     setOutput();
 }
 
-var geocoder;
-var markers = [];
-var map;
 
 function myMap(long, lat) {
     geocoder = new google.maps.Geocoder();
@@ -24,17 +23,14 @@ function myMap(long, lat) {
     markers.setMap(map);
 
     google.maps.event.addListener(map, 'click', function (e) {
-        if(markers.size < 2){
-            markers.setMap(null);
-        }
+        markers.setMap(null);
         markers = new google.maps.Marker({position: e.latLng});
         markers.setMap(map);
-        $("#latitude").val(markers.getPosition().lat());
-        $("#longitude").val(markers.getPosition().lng());
         geocodePosition(markers.getPosition())
     });
 
 }
+$("#addMarker").click(addMarker);
 
 function geocodePosition(pos){
     geocoder.geocode({
@@ -43,30 +39,35 @@ function geocodePosition(pos){
         if (responses && responses.length > 0){
             markers.formatted_address = responses[0].formatted_address;
         }
-        $("#address").val(markers.formatted_address);
-        $("#longLat").val(markers.getPosition().lat() + ":" + markers.getPosition().lng());
+        $("#startAddress").val(markers.formatted_address);
+        $("#startLongLat").val(markers.getPosition().lat() + ":" + markers.getPosition().lng());
     })
 }
 
 
 function addMarker() {
-    var address = $('#address').val(),
-        coordinates = $('#longLat').val().trim().split(':');
-    myMap(coordinates[0], coordinates[1]);
-    setInput(address, coordinates);
+    var startAddress = $('#startAddress').val(),
+        startCoordinates = $('#startLongLat').val().trim().split(':'),
+        endAddress = $('#endAddress').val(),
+        endCoordinates = $('#endLongLat').val().trim().split(':');
+    myMap(startCoordinates[0], startCoordinates[1]);
+    setInput(startAddress, startCoordinates, endAddress, endCoordinates);
 }
 
 
-function setInput(address, coordinates) {
+function setInput(startAddress, startCoordinates, endAddress, endCoordinates) {
     $.ajax({
         url: "http://localhost:8080/coordinates",
         dataType: 'json',
         type: 'post',
         contentType: 'application/json',
-        data: '{ "coordinateStart":' + '"' + address + '"' + ', "coordinateEnd":' + '"' + coordinates + '"' + '}',
+        data:
+        '{ "addressStart":' + '"' + startAddress + '"' + ', "coordinateStart":' + '"' + startCoordinates + '"' +
+        '"addressEnd":' + '"' + endAddress + '"' + ', "coordinateEnd":' + '"' + endCoordinates + '"' + '}',
         processData: false,
         success: function () {
-            console.log("Coordinates sent to API \n" + address + "  " + coordinates);
+            console.log("Start sent to API \n" + startAddress + "  " + startCoordinates);
+            console.log("End sent to API \n" + endAddress + "  " + endCoordinates);
             setOutput();
         },
         error: function (jqXhr, textStatus, errorThrown) {
@@ -84,7 +85,10 @@ function setOutput() {
         success: function (data) {
             $("#output").html(null);
             $.each(data, function (index, value) {
-                $('#output').append('<dt id="coords">' + value.coordinateStart + " : " + value.coordinateEnd);
+                $('#output')
+                    .append('<dt id="coords">' + value.coordinateStart + " : " + value.coordinateEnd)
+                    .append('<span id="addressNames">' + value.addressStart + " - " + value.addressEnd + '</span>');
+
             });
         },
         error: function (jqXhr, textStatus, errorThrown) {
@@ -96,10 +100,11 @@ function setOutput() {
     $('#output').on('click', 'dt', function () {
         var click_text = $(this).text().split(' : ');
         $('#selected').html('<b>Selected: </b>' + click_text[0] + " : " + click_text[1]);
-        var coordinates = click_text[1].trim().split(',');
-        myMap(parseFloat(coordinates[0]), parseFloat(coordinates[1]));
+        myMap(parseFloat(click_text[0]), parseFloat(click_text[1]));
     });
+
 }
+
 
 
 
