49b5539a6d8484c1fbd98aeb4d5bf30262a56618
Vladdd97 <vladbantus02@gmail.com>
2018-01-23 17:25:45 +0200

    Front-end UPDATE

--- www/script/main.js
+++ www/script/main.js
@@ -1,109 +1,71 @@
 $(document).ready(main);
-var geocoder;
-var markers = [];
-var marker;
 var map;
-var directionsService;
-var directionsDisplay;
-var allDirectionsContainer;
-
+var markers = [];
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
 function main() {
-    directionsService = new google.maps.DirectionsService;
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    myMap(parseFloat(47.010921), parseFloat(28.840330));
-    setOutput();
-
+    initMap();
+    $("#showRoute").click(showRoute);
+    $("#deleteRoute").click(deleteRoute);
 }
 
+function showRoute() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    clearMarkers();
+}
 
-function myMap(long, lat) {
-    geocoder = new google.maps.Geocoder();
-    var myCenter = new google.maps.LatLng(long, lat);
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: myCenter,
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(mapCanvas, mapOptions);
+function deleteRoute() {
+    directionsDisplay.setDirections({routes: []});
+    setEmptyFields();
+}
+
+function setEmptyFields() {
+    $("#startAddress").val(null);
+    $("#endAddress").val(null);
+    $("#startLatLng").val(null);
+    $("#endLatLng").val(null);
+}
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {lat: 47.00989485019684, lng: 28.840969763696194 }
+    });
     directionsDisplay.setMap(map);
-    marker = new google.maps.Marker({
-        position: myCenter,
-        draggable: true
-    });
-    markers.push(marker);
-    clearMapMarkers();
 
-    google.maps.event.addListener(map, 'click', function (e) {
-
-
-        if (markers.length !== 3) {
-
-            marker = new google.maps.Marker({
-                position: e.latLng,
-                draggable: true
-            });
-            markers.push(marker);
-            setMapOnAll(map);
-
-            if ($('#startAddress').val() !== '' && $('#endAddress').val() !== '') {
-                console.log('GOT INTO DIRECTIONS IF');
-                console.log($('#startAddress').val());
-                console.log($('#endAddress').val());
-                calculateAndDisplayRoute(directionsService, directionsDisplay);
-            }
-
-
-        } else {
-            setMapOnAll(null);
-            markers = [];
-            $("#startAddress").val(null);
-            $("#endAddress").val(null);
-            $("#startLongLat").val(null);
-            $("#endLongLat").val(null);
-        }
+    map.addListener('click', function(e) {
+        addMarker(e.latLng, map);
     });
 }
 
-$("#addMarker").click(addMarker);
+function addMarker(latLng, map) {
+    if (markers.length > 1){
+        alert("clear !");
+        clearMarkers();
+        setEmptyFields();
+    }
+    markers.push(new google.maps.Marker({
+        position: latLng,
+        map: map
+    }));
+    geocodePosition(markers.length-1);
+}
 
 function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
-        if (map != null) {
-            geocodePosition(i);
-        }
     }
 }
 
-function clearMapMarkers() {
+//delete all markers
+function clearMarkers() {
     setMapOnAll(null);
     markers = [];
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route({
-        origin: $('#startAddress').val(),
-        destination: $('#endAddress').val(),
-        travelMode: 'DRIVING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            console.log('success');
-            console.log($('#startAddress').val());
-            console.log($('#endAddress').val());
-            directionsDisplay.setDirections(response);
-            setMapOnAll(null);
-
-        } else {
-            console.log('insuccess');
-            console.log($('#startAddress').val());
-            console.log($('#endAddress').val());
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
-}
-
+// Fill our form with adress and coordinates
 function geocodePosition(i) {
+    var geocoder = new google.maps.Geocoder();
     geocoder.geocode({
         latLng: markers[i].getPosition()
     }, function (responses) {
@@ -112,28 +74,46 @@ function geocodePosition(i) {
         }
         if (i === 0) {
             $("#startAddress").val(markers[i].formatted_address);
-            $("#startLongLat").val(markers[i].getPosition().lat() + ":" + markers[i].getPosition().lng());
+            $("#startLatLng").val(markers[i].getPosition().lat() + ":" + markers[i].getPosition().lng());
         }
         if (i === 1) {
             $("#endAddress").val(markers[i].formatted_address);
-            $("#endLongLat").val(markers[i].getPosition().lat() + ":" + markers[i].getPosition().lng());
+            $("#endLatLng").val(markers[i].getPosition().lat() + ":" + markers[i].getPosition().lng());
         }
     })
 }
 
+// Show Route
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    //console.log(markers[0].formatted_address);
+    //console.log(markers[1].formatted_address);
+    directionsService.route({
+        origin: $("#startAddress").val() ,
+        destination: $("#endAddress").val() ,
+        travelMode: 'DRIVING'
+    }, function (response, status) {
+        if (status === "OK") {
+            console.log('success');
+            console.log($('#startAddress').val());
+            console.log($('#endAddress').val());
+            directionsDisplay.setDirections(response);
 
-function addMarker() {
-    var startAddress = $('#startAddress').val(),
-        startCoordinates = $('#startLongLat').val().trim(),
-        endAddress = $('#endAddress').val(),
-        endCoordinates = $('#endLongLat').val().trim();
-    setInput(startAddress, startCoordinates, endAddress, endCoordinates);
-
+        } else {
+            console.log('insuccess');
+            console.log($('#startAddress').val());
+            console.log($('#endAddress').val());
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+    setInput();
 }
 
 
-function setInput(startAddress, startCoordinates, endAddress, endCoordinates) {
-
+function setInput() {
+    var startAddress = $("#startAddress").val(),
+        endAddress = $('#endAddress').val(),
+        startCoordinates = $("#startLatLng").val().trim(),
+        endCoordinates = $("#endLatLng").val().trim();
     $.ajax({
         url: "http://localhost:8080/coordinates",
         dataType: 'json',
@@ -141,10 +121,10 @@ function setInput(startAddress, startCoordinates, endAddress, endCoordinates) {
         contentType: 'application/json',
         data:
         '{ ' +
-        '"addressStart":'       + '"' + startAddress        + '", ' +
-        '"coordinateStart":'    + '"' + startCoordinates    + '", ' +
-        '"addressEnd":'         + '"' + endAddress          + '", ' +
-        '"coordinateEnd":'      + '"' + endCoordinates      + '"' +
+        '"addressStart":' + '"' + startAddress + '", ' +
+        '"coordinateStart":' + '"' + startCoordinates + '", ' +
+        '"addressEnd":' + '"' + endAddress + '", ' +
+        '"coordinateEnd":' + '"' + endCoordinates + '"' +
         '}',
         processData: false,
         success: function () {
@@ -171,7 +151,7 @@ function setOutput() {
             $("#output").html(null);
             $.each(data, function (index, value) {
                 allDirectionsContainer = data;
-                $('#output')
+                $("#output")
                     .append('<dt id="addresses">' + value.addressStart + " - " + value.addressEnd);
             });
         },
@@ -184,7 +164,7 @@ function setOutput() {
 }
 
 $('#output').on('click', 'dt', function () {
-    var click_text = $(this).text().split(' - ');
+    var click_text = $(this).text().split(" - ");
     $('#startAddress').val(click_text[0]);
     $('#endAddress').val(click_text[1]);
     $('#selected').html('<b>Selected: </b>' + click_text[0] + " : " + click_text[1]);
@@ -193,15 +173,15 @@ $('#output').on('click', 'dt', function () {
 
 $('#output').on('contextmenu', 'dt', function () {
     var click_text = $(this).text().split(' - ');
-    $.each(allDirectionsContainer, function(index, value){
-        if (click_text[0] === value.addressStart && click_text[1] === value.addressEnd){
+    $.each(allDirectionsContainer, function (index, value) {
+        if (click_text[0] === value.addressStart && click_text[1] === value.addressEnd) {
             deleteDirection(value.id);
         }
 
     })
 });
 
-function deleteDirection(id){
+function deleteDirection(id) {
 
     $.ajax({
         url: "http://localhost:8080/coordinates/" + id,
@@ -214,11 +194,4 @@ function deleteDirection(id){
         }
     });
 
-
 }
-
-
-
-
-
-
